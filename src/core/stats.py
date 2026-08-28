2fdf02f8d13d9b6ddb4ade70e4c9a0a48ac40021
Raul Plesa <raul.plesa@gmail.com>
2026-08-28 12:49:26 +0200

    Added guild and user tables to db

--- src/core/stats.py
+++ src/core/stats.py
@@ -14,9 +14,30 @@ def init_db():
             )
         """)
 
+        db.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                user_id TEXT PRIMARY KEY,
+                username TEXT
+            )
+        """)
+
+        db.execute("""
+            CREATE TABLE IF NOT EXISTS guilds (
+                guild_id TEXT PRIMARY KEY,
+                name TEXT
+            )
+        """)
+        db.execute("""
+            INSERT INTO guilds (guild_id, name)
+            VALUES ('None', 'DM')
+            ON CONFLICT(guild_id) DO NOTHING
+        """)
+
+
 def increment_command(guild_id: str, author_id: str, command: str):
     if not guild_id:
         guild_id = "None"
+    
     with sqlite3.connect(DB_PATH) as db:
         db.execute("""
             INSERT INTO command_stats (guild_id, author_id, command, count)
