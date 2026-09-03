cb854252b41fa615960cf4961a9b87e48738be4b
= <raul.plesa@gmail.com>
2026-09-03 16:10:12 +0200

    Fixed who command (Lola Hayes excluded)

--- src/who/who.py
+++ src/who/who.py
@@ -3,6 +3,8 @@ from src.core.cards_db import cards
 from src.core.formatting import create_embed, format_text
 from src.core.translator import locale as _
 from src.who.utils import match_investigator_deck_options, filter_by_classes
+from config import ARKHAM_BUILD
+from src.core.arkhambuild import LocalizedAttribute as attr
 
 
 def resolve_search_who(array, guild_id="None"):
@@ -11,7 +13,7 @@ def resolve_search_who(array, guild_id="None"):
         card = array[0]
         who_can_take = []
         who_cannot_take = []
-        investigators = cards.get_investigators()
+        investigators = (inv for inv in cards.get_investigators() if inv["id"] != '90087' and inv["id"] != '03006' and "-" not in inv["id"])
         if "xp" in card:
             for inv in investigators:
                 result = match_investigator_deck_options(inv, card)
@@ -32,11 +34,11 @@ def resolve_search_who(array, guild_id="None"):
 def format_who(card, array, positive=True, guild_id="None"):
     """Formats the who command for a card."""
     neg_text = "" if positive else "_neg"
-    title = f"{_(f'ahWho_title{neg_text}')}: {card['name']}{taboo.format_xp(card)}"
+    title = f"{_(f'ahWho_title{neg_text}')}: {card[attr.NAME.get(card)]}{taboo.format_xp(card)}"
     description = ""
 
     if len(array) == 0 and not positive:
-        title = f"{_('ahWho_title')}: {card['name']}{taboo.format_xp(card)}"
+        title = f"{_('ahWho_title')}: {card[attr.NAME.get(card)]}{taboo.format_xp(card)}"
         description = f"{_('ahWho_everyone')}"
         return create_embed(title=title, description=description, c=card)
 
@@ -52,7 +54,7 @@ def format_who(card, array, positive=True, guild_id="None"):
 
     for faction, investigators in classes.items():
         if investigators:
-            names = [c["name"] for c in investigators]
+            names = [f"{c[attr.NAME.get(c)]}[↗]({ARKHAM_BUILD}/card/{c["code"]})" for c in investigators]
             description = ", ".join(names)
             title = f"{format_text('[' + faction + ']', guild_id)}{_(faction)} ({len(investigators)}):"
             embed.add_field(name=title, value=description)
